c47f4c099fb4792b08898baf097e7dfdd525ad61
minje cho <minje@ajou.ac.kr>
2021-11-16 01:24:38 +0900

    feat: html tag ID 상수 추가

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const CAR_NAMES_INPUT = 'car-names-input';
+export const CAR_NAMES_BUTTON = 'car-names-submit';
+
+export const RACING_COUNT_DIV = 'racing-count-container';
+export const RACING_COUNT_INPUT = 'racing-count-input';
+export const RACING_COUNT_BUTTON = 'racing-count-submit';
+
+export const RESULT_DIV = 'result-container';
+export const RESULT_CONTENT = 'result-content';

979c5e3723e94d2e0663d680cf116d703bf36e80
minje cho <minje@ajou.ac.kr>
2021-11-16 02:20:52 +0900

    feat: 시도 횟수 및 결과 창 숨기기

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import {
+  CAR_NAMES_CONTAINER,
   CAR_NAMES_INPUT,
   CAR_NAMES_BUTTON,
+  RACING_COUNT_CONTAINER,
   RACING_COUNT_INPUT,
   RACING_COUNT_BUTTON,
+  RESULT_CONTAINER,
+  RESULT_CONTENT,
 } from './utils/constants.js';
+import { hideNode, showNode } from './utils/domUtil.js';
 
 export default function RacingCarGame() {
-  const elements = getElements();
-}
-const getElements = () => {
-  return {
-    carNamesInput: document.querySelector(CAR_NAMES_INPUT),
-    carNamesSubmit: document.querySelector(CAR_NAMES_BUTTON),
-    racingCountInput: document.querySelectorAll(RACING_COUNT_INPUT),
-    racingCountSubmit: document.querySelectorAll(RACING_COUNT_BUTTON),
+  const getElements = () => {
+    return {
+      carNamesContainer: document.getElementById(CAR_NAMES_CONTAINER),
+      carNamesInput: document.getElementById(CAR_NAMES_INPUT),
+      carNamesSubmit: document.getElementById(CAR_NAMES_BUTTON),
+      racingCountContainer: document.getElementById(RACING_COUNT_CONTAINER),
+      racingCountInput: document.getElementById(RACING_COUNT_INPUT),
+      racingCountSubmit: document.getElementById(RACING_COUNT_BUTTON),
+      resultContainer: document.getElementById(RESULT_CONTAINER),
+      resultContent: document.getElementById(RESULT_CONTENT),
+    };
   };
-};
+  const initDom = () => {
+    hideNode(elements.racingCountContainer);
+    hideNode(elements.resultContainer);
+  };
+
+  const elements = getElements();
+  initDom();
+}
+
 new RacingCarGame();
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,10 @@
+export const CAR_NAMES_CONTAINER = 'car-names-container';
 export const CAR_NAMES_INPUT = 'car-names-input';
 export const CAR_NAMES_BUTTON = 'car-names-submit';
 
-export const RACING_COUNT_DIV = 'racing-count-container';
+export const RACING_COUNT_CONTAINER = 'racing-count-container';
 export const RACING_COUNT_INPUT = 'racing-count-input';
 export const RACING_COUNT_BUTTON = 'racing-count-submit';
 
-export const RESULT_DIV = 'result-container';
+export const RESULT_CONTAINER = 'result-container';
 export const RESULT_CONTENT = 'result-content';

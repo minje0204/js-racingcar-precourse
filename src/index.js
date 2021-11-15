c7edcb3cf0073cb39ea50a1ea37edd57e9ddd04d
minje cho <minje@ajou.ac.kr>
2021-11-16 01:59:55 +0900

    feat: element ID 쿼리 수정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import {
   CAR_NAMES_INPUT,
   CAR_NAMES_BUTTON,
-  RACING_COUNT_DIV,
   RACING_COUNT_INPUT,
   RACING_COUNT_BUTTON,
-  RESULT_DIV,
-  RESULT_CONTENT,
 } from './utils/constants.js';
 
 export default function RacingCarGame() {
@@ -13,10 +10,10 @@ export default function RacingCarGame() {
 }
 const getElements = () => {
   return {
-    carNamesInput: document.querySelector('#CAR_NAMES_INPUT'),
-    carNamesSubmit: document.querySelector('#CAR_NAMES_BUTTON'),
-    racingCountInput: document.querySelectorAll('#RACING_COUNT_INPUT'),
-    racingCountSubmit: document.querySelectorAll('#RACING_COUNT_BUTTON'),
+    carNamesInput: document.querySelector(CAR_NAMES_INPUT),
+    carNamesSubmit: document.querySelector(CAR_NAMES_BUTTON),
+    racingCountInput: document.querySelectorAll(RACING_COUNT_INPUT),
+    racingCountSubmit: document.querySelectorAll(RACING_COUNT_BUTTON),
   };
 };
 new RacingCarGame();

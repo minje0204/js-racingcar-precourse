ab5b75f318e26262ca861e37c8fd573d7ca10c7f
minje cho <minje@ajou.ac.kr>
2021-11-16 01:58:01 +0900

    feat: element 불러오기

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import CarRacingGame from "./components/CarRacingGame.js";
+import {
+  CAR_NAMES_INPUT,
+  CAR_NAMES_BUTTON,
+  RACING_COUNT_DIV,
+  RACING_COUNT_INPUT,
+  RACING_COUNT_BUTTON,
+  RESULT_DIV,
+  RESULT_CONTENT,
+} from './utils/constants.js';
 
-new CarRacingGame(document.querySelector("#app"));
+export default function RacingCarGame() {
+  const elements = getElements();
+}
+const getElements = () => {
+  return {
+    carNamesInput: document.querySelector('#CAR_NAMES_INPUT'),
+    carNamesSubmit: document.querySelector('#CAR_NAMES_BUTTON'),
+    racingCountInput: document.querySelectorAll('#RACING_COUNT_INPUT'),
+    racingCountSubmit: document.querySelectorAll('#RACING_COUNT_BUTTON'),
+  };
+};
+new RacingCarGame();

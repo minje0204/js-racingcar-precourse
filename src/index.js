61b262a54d677b98ef7e5d0adc69d8a158d8f309
minje cho <minje@ajou.ac.kr>
2021-11-16 22:23:10 +0900

    feat: 자동차 이름 유효성 검사 및 submit event 추가

--- src/index.js
+++ src/index.js
@@ -11,25 +11,35 @@ import {
 import { hideNode, showNode } from './utils/domUtil.js';
 
 export default function RacingCarGame() {
-  const getElements = () => {
-    return {
-      carNamesContainer: document.getElementById(CAR_NAMES_CONTAINER),
-      carNamesInput: document.getElementById(CAR_NAMES_INPUT),
-      carNamesSubmit: document.getElementById(CAR_NAMES_BUTTON),
-      racingCountContainer: document.getElementById(RACING_COUNT_CONTAINER),
-      racingCountInput: document.getElementById(RACING_COUNT_INPUT),
-      racingCountSubmit: document.getElementById(RACING_COUNT_BUTTON),
-      resultContainer: document.getElementById(RESULT_CONTAINER),
-      resultContent: document.getElementById(RESULT_CONTENT),
-    };
-  };
-  const initDom = () => {
-    hideNode(elements.racingCountContainer);
-    hideNode(elements.resultContainer);
-  };
+  const carNamesContainer = document.getElementById(CAR_NAMES_CONTAINER);
+  const carNamesInput = document.getElementById(CAR_NAMES_INPUT);
+  const carNamesSubmit = document.getElementById(CAR_NAMES_BUTTON);
+  const racingCountContainer = document.getElementById(RACING_COUNT_CONTAINER);
+  const racingCountInput = document.getElementById(RACING_COUNT_INPUT);
+  const racingCountSubmit = document.getElementById(RACING_COUNT_BUTTON);
+  const resultContainer = document.getElementById(RESULT_CONTAINER);
+  const resultContent = document.getElementById(RESULT_CONTENT);
 
-  const elements = getElements();
-  initDom();
+  initDom(racingCountContainer, resultContainer);
+
+  carNamesSubmit.addEventListener('click', () => handleCarNames(carNamesInput));
 }
+
+const initDom = (racingCountContainer, resultContainer) => {
+  hideNode(racingCountContainer);
+  hideNode(resultContainer);
+};
+
+const handleCarNames = input => {
+  const carNames = input.value.split(',');
+
+  if (carNames.length <= 1 || carNames.length != new Set(carNames).size) {
+    alert('잘못된 형식');
+    input.value = '';
+    return input.focus();
+  }
+
+  generateCars(); //todo
+};
 
 new RacingCarGame();
